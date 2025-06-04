96ab1587e5e8ebc3ca82cae94cbda82d8f3d78e2
ukuleleadventure2 <udzura1093@gmail.com>
2025-06-04 15:52:52 +0900

    Bootstrap macOS player project

---
+++ SimpleMoviewApp/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "SimpleMoviewApp",
+    platforms: [
+        .macOS(.v15)
+    ],
+    products: [
+        .executable(name: "SimpleMoviewApp", targets: ["SimpleMoviewApp"])
+    ],
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "SimpleMoviewApp",
+            path: "Sources"
+        )
+    ]
+)
